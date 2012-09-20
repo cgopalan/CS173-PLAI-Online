c9d11839e90abecc61f9d6489732ac7ea5de8ed0
cgopalan <cgopalan@Chandrakant-Gopalans-MacBook.local>
2012-09-20 17:47:27 -0400

    Added structure mutation capability (mutation to object-like structures, ie boxes)

---
+++ interp5.rkt
@@ -0,0 +1,129 @@
+#lang plai-typed
+
+(define-type-alias Location number)
+
+(define-type Binding
+  [bind (name : symbol) (val : Location)])
+
+(define-type-alias Env (listof Binding))
+(define mt-env empty)
+(define extend-env cons)
+
+(define-type Storage
+  [cell (location : Location) (val : Value)])
+
+(define-type-alias Store (listof Storage))
+(define mt-store empty)
+(define override-store cons)
+
+(define-type ExprC
+  [numC (n : number)]
+  [plusC (l : ExprC) (r : ExprC)]
+  [multC (l : ExprC) (r : ExprC)]
+  [idC (s : symbol)]
+  [appC (f : ExprC) (a : ExprC)]
+  [lamC (arg : symbol) (body : ExprC)]
+  [boxC (arg : ExprC)]
+  [unboxC (arg : ExprC)]
+  [setboxC (b : ExprC) (v : ExprC)]
+  [seqC (b1 : ExprC) (b2 : ExprC)])
+
+(define-type Value
+  [numV (n : number)]
+  [closV (arg : symbol) (body : ExprC) (env : Env)]
+  [boxV (l : Location)])
+
+(define-type Result
+  [v*s (v : Value) (s : Store)])
+
+(define (interp [e : ExprC] [env : Env] [sto : Store]) : Result
+  (type-case ExprC e
+    [numC (n) (v*s (numV n) sto)]
+    [plusC (l r) (type-case Result (interp l env sto)
+                   [v*s (v-l s-l)
+                        (type-case Result (interp r env s-l)
+                          [v*s (v-r s-r)
+                               (v*s (num+ v-l v-r) s-r)])])]
+    [multC (l r) (type-case Result (interp l env sto)
+                   [v*s (v-l s-l)
+                        (type-case Result (interp r env s-l)
+                          [v*s (v-r s-r)
+                               (v*s (num* v-l v-r) s-r)])])]
+    [idC (n) (v*s (fetch (lookup n env) sto) sto)]
+    [appC (f a) (type-case Result (interp f env sto)
+                  [v*s (v-f s-f)
+                       (type-case Result (interp a env s-f)
+                         [v*s (v-a s-a)
+                              (let ([where (new-loc)])
+                                (interp (closV-body v-f)
+                                        (extend-env (bind (closV-arg v-f)
+                                                          where)
+                                                    (closV-env v-f))
+                                        (override-store (cell where v-a)
+                                                        s-a)))])])]
+    [lamC (a b) (v*s (closV a b env) sto)]
+    [boxC (a) (type-case Result (interp a env sto)
+                [v*s (v-a s-a)
+                     (let ([where (new-loc)])
+                       (v*s (boxV where)
+                            (override-store (cell where v-a)
+                                            s-a)))])]
+    [unboxC (a) (type-case Result (interp a env sto)
+                  [v*s (v-a s-a)
+                       (v*s (fetch (boxV-l v-a) s-a) s-a)])]
+    [setboxC (b v) (type-case Result (interp b env sto)
+                     [v*s (v-b s-b)
+                          (type-case Result (interp v env s-b)
+                            [v*s (v-v s-v)
+                                 (v*s v-v
+                                      (override-store 
+                                       (cell (boxV-l v-b) v-v)
+                                       s-v))])])]
+    [seqC (b1 b2) (type-case Result (interp b1 env sto)
+                    [v*s (v-b1 s-b1)
+                         (interp b2 env s-b1)])]
+    )
+  )
+
+(define (num+ [l : Value] [r : Value]) : Value
+  (cond
+    [(and (numV? l) (numV? r))
+     (numV (+ (numV-n l) (numV-n r)))]
+    [else 
+     (error 'num+ "one argument was not a number")]))
+
+(define (num* [l : Value] [r : Value]) : Value
+  (cond
+    [(and (numV? l) (numV? r))
+     (numV (* (numV-n l) (numV-n r)))]
+    [else 
+     (error 'num+ "one argument was not a number")]))
+
+(define (lookup [for : symbol] [env : Env]) : Location
+  (cond
+    [(empty? env) (error 'lookup "name not found")]
+    [else (cond
+            [(symbol=? for (bind-name (first env)))
+             (bind-val (first env))]
+            [else (lookup for (rest env))])]))
+
+(define (fetch [loc : Location] [ sto : Store]) : Value
+  (cond
+    [(empty? sto) (error 'fetch "store not found")]
+    [else (cond
+            [(eq? loc (cell-location (first sto)))
+             (cell-val (first sto))]
+            [else (fetch loc (rest sto))])]))
+
+(define new-loc
+  (let ([n (box 0)])
+    (lambda ()
+      (begin
+        (set-box! n (add1 (unbox n)))
+        (unbox n)))))
+
+; tests
+(test (interp (boxC (numC 5)) mt-env mt-store) 
+      (v*s (boxV 1) (list (cell 1 (numV 5)))))
+(test (interp (unboxC (boxC (numC 5))) mt-env mt-store)
+      (v*s (numV 5) (list (cell 2 (numV 5)))))
